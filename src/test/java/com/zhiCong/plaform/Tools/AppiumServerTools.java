befb7dc8b40e8ed74f67ab3f11bc1d3c9bf2bb1d
zhicong xie <1392438023@qq.com>
2022-07-14 18:46:31 +0800

    hidden auto start appium service logic

--- src/test/java/com/zhiCong/plaform/Tools/AppiumServerTools.java
+++ src/test/java/com/zhiCong/plaform/Tools/AppiumServerTools.java
@@ -1,10 +1,10 @@
 //package com.zhiCong.Plaform.Tools;
 //
 //import io.appium.java_client.AppiumDriver;
+//import org.openqa.selenium.remote.DesiredCapabilities;
 //import io.appium.java_client.service.local.AppiumDriverLocalService;
 //import io.appium.java_client.service.local.AppiumServiceBuilder;
 //import io.appium.java_client.service.local.flags.GeneralServerFlag;
-//import org.openqa.selenium.remote.DesiredCapabilities;
 //
 //import java.io.IOException;
 //import java.net.MalformedURLException;
@@ -12,62 +12,72 @@
 //import java.net.URL;
 //
 //public class AppiumServerTools {
-//
-//  private static AppiumDriver driver;
 //  private AppiumDriverLocalService service;
 //  private AppiumServiceBuilder builder;
+//  private DesiredCapabilities des;
+//  private AppiumDriver appiumDriver;
 //
-//  public void startAppiumServer(int proxy) {
+//  public void startServer(int proxy) {
+//    // Set Capabilities
+//    des = new DesiredCapabilities();
+//        des.setCapability("app",
+//     "/Users/automatiautomationon/Downloads/pizzahut_crm_android-app-pizzahut-uat-release-1.2.3-84-20180110.apk");
+//          des.setCapability("platformName", "Android");
+//          des.setCapability("platformVersion", "11.0");
+//          des.setCapability("deviceName", "emulator-5554");
+//          des.setCapability("automationName", "UiAutomator2");
+//    //    des.setCapability("app", "/Users/automatiautomationon/Downloads/PizzaHut_CRM_UAT.app");
+//    //    des.setCapability("platformName", "iOS");
+//    //    des.setCapability("platformVersion", "15.5");
+//    //    des.setCapability("deviceName", "iPhone 13");
+//    //    des.setCapability("automationName", "XCUITest");
+//    // Build the Appium service
 //    builder = new AppiumServiceBuilder();
 //    builder.withIPAddress("127.0.0.1");
 //    builder.usingPort(proxy);
+//    builder.withCapabilities(des);
 //    builder.withArgument(GeneralServerFlag.SESSION_OVERRIDE);
 //    builder.withArgument(GeneralServerFlag.LOG_LEVEL, "error");
+//    // Start the server with the builder
 //    service = AppiumDriverLocalService.buildService(builder);
 //    service.start();
-//  }
 //
-//  public void stopAppiumServer() {
-//    service.stop();
-//  }
-//
-//  public void connectAppiumServer(int proxy) {
-//    DesiredCapabilities des = new DesiredCapabilities();
-//    des.setCapability(
-//        "app",
-//        "/Users/automatiautomationon/Downloads/pizzahut_crm_android-app-pizzahut-uat-release-1.2.3-84-20180110.apk");
-//    des.setCapability("platformName", "Android");
-//    des.setCapability("platformVersion", "10.0");
-//    des.setCapability("deviceName", "emulator-5554");
-//    des.setCapability("automationName", "UiAutomator2");
 //    try {
-//      driver = new AppiumDriver(new URL(String.format("http://127.0.0.1:%s/wd/hub", proxy)), des);
+//      appiumDriver =
+//          new AppiumDriver(new URL(String.format("http://127.0.0.1:%s/wd/hub", proxy)), des);
 //    } catch (MalformedURLException e) {
 //      e.printStackTrace();
 //    }
 //  }
 //
-//  public boolean checkIfServerIsRunnning(int port) {
+//  public void stopServer() {
+//    service.stop();
+//  }
+//
+//  public boolean checkIfServerIsRunning(int port) {
 //    boolean isServerRunning = false;
 //    ServerSocket serverSocket;
 //    try {
 //      serverSocket = new ServerSocket(port);
 //      serverSocket.close();
 //    } catch (IOException e) {
+//      // If control comes here, then it means that the port is in use
 //      isServerRunning = true;
+//    } finally {
+//      serverSocket = null;
 //    }
 //    return isServerRunning;
 //  }
 //
 //  public static void main(String[] args) {
 //    AppiumServerTools appiumServer = new AppiumServerTools();
-//    int port = 4723;
-//    if (!appiumServer.checkIfServerIsRunnning(port)) {
-//      appiumServer.startAppiumServer(port);
-//      appiumServer.connectAppiumServer(port);
-//      appiumServer.stopAppiumServer();
+//    int proxy = 4723;
+//    if (!appiumServer.checkIfServerIsRunning(proxy)) {
+//      appiumServer.startServer(proxy);
+//      System.out.println("success");
+//      appiumServer.stopServer();
 //    } else {
-//      System.out.println("Appium Server already running on Port - " + port);
+//      System.out.println("Appium Server already running on Port - " + proxy);
 //    }
 //  }
 //}
